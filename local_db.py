0585c929038eec463d2b84dcd9316aa70cd4596b
Sam Griffith <garfunkelnator@gmail.com>
2017-11-13 18:09:51 -0500

    Update local_db.py
    The local db for the GAME OF STUFF

--- local_db.py
+++ local_db.py
@@ -1,8 +1,26 @@
 from sqlalchemy import create_engine, Integer, Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-Base = declarative_base()
-Session = sessionmaker()
-session = Session()
-engine = create_engine('sqlite:///:memory:', echo=True)
 
+
+Base = declarative_base()
+engine = create_engine('sqlite:///:memory:', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+ 
+class Cards():
+  __tablename__ = 'cards'
+  
+  id = Column(Integer, primary_key=True)
+  card = Column(String)
+  
