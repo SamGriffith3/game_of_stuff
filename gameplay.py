8fcd702ef0835d0bbe93a1583f64c4b106d048ea
Sam Griffith <garfunkelnator@gmail.com>
2017-11-13 19:46:37 -0500

    Update gameplay.py

--- gameplay.py
+++ gameplay.py
@@ -1,4 +1,15 @@
+from random import *
 
+
+def choose_card():
+    used_cards = []
+    rating_filter = []
+    
+    if input("Include Rated G: y/n?") == "y" or "Y" rated_g = true 
+    if input("Include Rated T: y/n?") == "y" or "Y" rated_t = true 
+    if input("Include Rated R: y/n?") == "y" or "Y" rated_r = true 
+    c = random(session.query(cards).filter(cards.rated_g, ))
+    
 def flip_card():
     return 1
     # This is where the code to access card database is stored
